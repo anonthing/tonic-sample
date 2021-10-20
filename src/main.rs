f39958f3528a02c8ff711a3b19f5c82fca72da07
Siddartha Guthikonda <siddartha.gu@gmail.com>
2021-10-20 12:15:14 -0700

    spwan tokio tasks

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,71 @@
 use log::{error, info};
 use std::pin::Pin;
-use tokio_stream::Stream;
-use tonic::{Status, Request, Response};
 use std::time::SystemTime;
+use tokio_stream::Stream;
 use tonic::transport::Server;
+use tonic::{Request, Response, Status};
 pub mod tonic_sample {
     tonic::include_proto!("tonicsample");
 }
-use tonic_sample::QueryRequest;
-use tonic_sample::sample_service_server::{SampleService, SampleServiceServer};
-use tonic_sample::SampleResponse;
 use rand::Rng;
-use tokio::sync::mpsc::{Sender, Receiver};
 use std::thread;
-
+use tokio::sync::mpsc::{Receiver, Sender};
+use tonic_sample::sample_service_server::{SampleService, SampleServiceServer};
+use tonic_sample::QueryRequest;
+use tonic_sample::SampleResponse;
 
 #[derive(Default)]
 pub struct SampleTonicService;
 
-fn generate_response(sender: tokio::sync::mpsc::Sender<Result<SampleResponse, tonic::Status>>) {
+async fn generate_response(sender: tokio::sync::mpsc::Sender<Result<SampleResponse, tonic::Status>>) {
+    for i in 0..30 {
+        // 30 streams
+        let sender_clone = sender.clone();
 
-
-
-    for i in 0..30 { // 30 streams
-   //     let sender_clone = sender.clone();
-
-    //    thread::spawn(move ||{
-        let mut rng = rand::thread_rng();
-        let mut hashes = Vec::new();
-        for j in 0..400_000 { // 400k i64's. 3.2MB per stream. 96MB total
-            hashes.push(rng.gen::<i64>());
-        }
-        let response = SampleResponse {
-            hash: hashes
-        };
+        tokio::spawn(async move {
+            let mut rng = rand::thread_rng();
+            let mut hashes = Vec::new();
+            for j in 0..400_000 {
+                // 400k i64's. 3.2MB per stream. 96MB total
+                hashes.push(rng.gen::<i64>());
+            }
+            let response = SampleResponse { hash: hashes };
             println!("Sending {:?}", i);
-        sender.send(Ok(response));
-      // });
+            sender_clone.send(Ok(response));
+        });
     }
 }
 
 #[tonic::async_trait]
 impl SampleService for SampleTonicService {
     type GetResponseStream =
-    Pin<Box<dyn Stream<Item = Result<SampleResponse, Status>> + Send + Sync>>;
+        Pin<Box<dyn Stream<Item = Result<SampleResponse, Status>> + Send + Sync>>;
 
     async fn get_response(
         &self,
         request: Request<QueryRequest>,
     ) -> Result<Response<Self::GetResponseStream>, tonic::Status> {
         let r = request.into_inner();
-        let  (sender, receiver) = tokio::sync::mpsc::channel(5);
+        let (sender, receiver) = tokio::sync::mpsc::channel(5);
         let curr_time = SystemTime::now();
 
-        generate_response(sender);
-        println!("Time took to generate response {:?}", SystemTime::now().duration_since(curr_time).unwrap());
+        generate_response(sender).await;
+        println!(
+            "Time took to generate response {:?}",
+            SystemTime::now().duration_since(curr_time).unwrap()
+        );
         Ok(Response::new(Box::pin(
             tokio_stream::wrappers::ReceiverStream::new(receiver),
         )))
-
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     start_grpc_server().await.unwrap();
 
     Ok(())
 }
-
 
 async fn start_grpc_server() -> Result<(), Box<dyn std::error::Error>> {
     let mut hostname = local_ipaddress::get().unwrap();
@@ -81,12 +77,8 @@ async fn start_grpc_server() -> Result<(), Box<dyn std::error::Error>> {
     let svc = SampleServiceServer::new(sample_service).send_gzip();
     println!("Starting server on {:?}", hostname);
 
-    Server::builder()
-        .add_service(svc)
-        .serve(addr)
-        .await?;
+    Server::builder().add_service(svc).serve(addr).await?;
 
     println!("Started server on {:?}", hostname);
     Ok(())
 }
-
